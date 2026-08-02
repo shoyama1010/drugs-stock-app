95de9ec3f322df8f6141c12ccfa589698311171b
shoyama1010 <kochisk8@gmail.com>
2026-08-02 10:41:51 +0900

    Configure CORS for Bearer authentication

--- src/config/cors.php
+++ src/config/cors.php
@@ -15,16 +15,18 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+    ],
 
     'allowed_methods' => ['*'],
 
-    // 'allowed_origins' => [
-    //     'http://localhost:5173',
-    //     'https://drugsstock-frontend.vercel.app',
-    //     // 'https://drugstock-frontend-dh9jracrs-shoyama1010s-projects.vercel.app',
-    // ],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://drugsstock-frontend.vercel.app',
+        'https://drugstock-frontend-dh9jracrs-shoyama1010s-projects.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
